723d4e2321e31d0fdd30df8d8920078c53c3f707
Betul Zora <zorabtl@gmail.com>
2023-03-30 12:28:47 -0500

    US05_BZ completed

--- src/test/java/com/library/steps/US05_BZ_StepDefs.java
+++ src/test/java/com/library/steps/US05_BZ_StepDefs.java
@@ -1,27 +1,39 @@
 package com.library.steps;
 
 import com.library.pages.BorrowedBooksPage;
+import com.library.utility.DB_Util;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class US05_BZ_StepDefs {
-    BorrowedBooksPage borrowedBooksPage = new BorrowedBooksPage();
 
     @Given("Establish the database connection BZ")
     public void establish_the_database_connection_bz() {
         // this step is completed in the Hooks
 
     }
+
+    String mostPopularBookCategory;
     @When("I execute query to find most popular book genre BZ")
     public void i_execute_query_to_find_most_popular_book_genre_bz() {
-        borrowedBooksPage.popularityOfBook();
+        String query="select name from book_categories where id\n" +
+                " = (select book_category_id from books where books.id\n" +
+                " = (select book_id from (select book_id, count(*)\n" +
+                "from book_borrow group by book_id order by 2 desc limit 1) popularBook))"; // 1855
+        DB_Util.runQuery(query);
 
+        mostPopularBookCategory = DB_Util.getFirstRowFirstColumn();
+        System.out.println("mostPopularBookCategory = " + mostPopularBookCategory);
 
 
     }
     @Then("verify {string} is the most popular book genre. BZ")
-    public void verify_is_the_most_popular_book_genre_bz(String string) {
+    public void verify_is_the_most_popular_book_genre_bz(String expectedCategory) {
+        System.out.println("mostPopularBookCategory = " + mostPopularBookCategory);
+        System.out.println("expectedCategory = " + expectedCategory);
+        Assert.assertEquals(expectedCategory,mostPopularBookCategory);
 
     }
 
--- src/test/resources/library.sql
+++ src/test/resources/library.sql
@@ -25,3 +25,17 @@ select book_id from book_borrow where is_returned=0;
 
 select * from book_categories;
 select book_category_id from books where id=912;
+
+select bc.name, count(*) from book_borrow bb inner join books b on b.id = bb.book_id
+                                             inner join book_categories bc on b.book_category_id = bc.id
+group by bc.name
+order by count(*) desc ;
+
+
+-- Query for most popular book catergory
+
+select name from book_categories where id
+ = (select book_category_id from books where books.id
+ = (select book_id from (select book_id, count(*)
+from book_borrow group by book_id order by 2 desc limit 1) popularBook));
+
