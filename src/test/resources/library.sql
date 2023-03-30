c7d1125fa3721650b3ade11bb50f2d70bc52d7b8
Betul Zora <zorabtl@gmail.com>
2023-03-30 18:40:28 -0500

    US05_BZ completed

--- src/test/resources/library.sql
+++ src/test/resources/library.sql
@@ -19,21 +19,8 @@ select * from users;
 select count(*) from book_borrow
 where is_returned=0;
 
-select book_id, planned_return_date, returned_date, is_returned from book_borrow;
-
-select book_id from book_borrow where is_returned=0;
-
-select * from book_categories;
-select book_category_id from books where id=912;
-
-select bc.name, count(*) from book_borrow bb inner join books b on b.id = bb.book_id
-                                             inner join book_categories bc on b.book_category_id = bc.id
-group by bc.name
-order by count(*) desc ;
-
-
--- Query for most popular book catergory
-
+-- US 05
+-- Query for most popular book category
 select name from book_categories where id
  = (select book_category_id from books where books.id
  = (select book_id from (select book_id, count(*)
